2f234d94b959ad57d2ae61972d633482c8485d69
Sergey <itog24>
2025-12-18 15:34:20 +0500

    fixes function get date

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,4 @@
+import re
 from typing import Union
 from masks import get_mask_card_number
 from masks import get_mask_account
@@ -7,22 +8,26 @@ def mask_account_card(number_account_card: Union[int, str]) -> Union[int, str]:
     """Функция, маскирующая номер счета и номер карты"""
     if "Счет" in number_account_card:
         return f"Cчет {get_mask_account(number_account_card)}"
-    elif "Maestro" in number_account_card:
-        return f"Maestro {get_mask_card_number(number_account_card[-16:])}"
-    elif "MasterCard" in number_account_card:
-        return f"MasterCard {get_mask_card_number(number_account_card[-16:])}"
-    elif "Visa Classic" in number_account_card:
-        return f"Visa Classic {get_mask_card_number(number_account_card[-16:])}"
-    elif "Visa Platinum" in number_account_card:
-        return f"Visa Platinum {get_mask_card_number(number_account_card[-16:])}"
-    elif "Visa Gold" in number_account_card:
-        return f"Visa Gold {get_mask_card_number(number_account_card[-16:])}"
-
+    elif "Счет" not in number_account_card:
+        return f"{number_account_card[:-16]}{get_mask_card_number(number_account_card[-16:])}"
+  
 
 def get_date(str_with_date: str) -> str:
     """Функция вывода даты в корректный формат"""
-    year = str_with_date[:4]
-    month = str_with_date[5:7]
-    day = str_with_date[8:10]
-    format_date = f"{day}.{month}.{year}"
-    return format_date
+    pattern = r'(\d{4})-(\d{2})-(\d{2})'
+    match = re.search(pattern, str_with_date)
+    year, month, day = match.groups()
+    return f"{day}.{month}.{year}"
+
+
+def get_date(date_string: str) -> str:
+    pattern = r'(\d{2,4})-(\d{2})-(\d{2,4})'
+    match = re.search(pattern, date_string)
+    part1, part2, part3 = match.groups()
+    if len(part1) == 4:
+        year, month, day = part1, part2, part3
+    else:
+        day, month, year = part1, part2, part3
+    return f"{day}.{month}.{year}"
+
+    
