7049578c8bc6a6061e25c02a1082040056a8c3ce
Sergey <itog24>
2025-12-19 16:52:41 +0500

    corrections after verification

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,15 @@
-def filter_by_state(list_with_data: list, state="EXECUTED") -> list:
+from typing import Dict, Any, List
+
+
+def filter_by_state(list_with_data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Функция для фильтрации списка по значению state"""
-    filtered_list = []
+    filtered_list: List[Dict[str, Any]] = []
     for element_of_dict in list_with_data:
-        for value in element_of_dict.values():
-            if value == state:
-                filtered_list.append(element_of_dict)
+        if element_of_dict.get("state") == state:
+            filtered_list.append(element_of_dict)
     return filtered_list
 
 
-def sort_by_date(list_with_data: list, sorting_order=True):
+def sort_by_date(list_with_data: List[Dict[str, Any]], sorting_order: bool = True) -> List[Dict[str, Any]]:
     """Функция для сортировки списка по дате, значение по умолчанию задано - по убыванию"""
     return sorted(list_with_data, key=lambda x: x["date"], reverse=sorting_order)
